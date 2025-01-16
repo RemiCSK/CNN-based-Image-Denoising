1d4066f5a9ab12faa59f864f9557e27126fbe3c0
RemiCSK <remi.calvet1@gmail.com>
2025-01-16 21:27:19 +0100

    define architecture FFDNet inspired model by adding a noise map input

--- every_model_architecture.py
+++ every_model_architecture.py
@@ -1,10 +1,11 @@
 import torch.nn as nn
-
+import torch
 ###### PUT ARCHITECTURE OF EACH MODEL HERE AS A CLASS ######
 
 
 ###### Define a class for the small_mnist_model #####   # we use classes to export and import the model easily
 # Smaller model for MMNIST dataset, this is the same model as big_model but with less intermediate convolution layers
+# THis model DOES NOT take a noise map as input. It just down samples, do convolutions and reverse the down sample operation.
 
 class Small_MNIST(nn.Module):
 
@@ -29,3 +30,35 @@ class Small_MNIST(nn.Module):
 )
     def forward(self, x):
         return self.model(x)
+
+
+###### Define a class for FFDNet_inspired_small_mnist model #####   # we use classes to export and import the model easily
+# This is a very similar model to Small_mnist_model but we add a noise map as input, inspired by what is done in FFDNet
+
+
+class FFDNet_inspired_small_mnist(nn.Module):
+
+    def __init__(self):
+        super(Small_MNIST, self).__init__()
+        self.model = nn.Sequential(
+            # REVERSIBLE DOWN SAMPLING is done in the forward function to apply it only to images and not the noise map
+            # First convolution layer
+            nn.Conv2d(in_channels=5, out_channels=64, kernel_size=3, padding=1),   # 4+1=5 input channels because we will add a noise map
+            nn.ReLU(inplace=True),
+            # 2 convolution layers with batch normalization and ReLU
+            *[nn.Sequential(
+            nn.Conv2d(in_channels=64, out_channels=64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True)) for _ in range(2)],
+            # Final convolution layer
+            nn.Conv2d(in_channels=64, out_channels=4, kernel_size=3, padding=1),
+            # ADD THE REVERSE OF THE DOWN SAMPLING
+            nn.PixelShuffle(2)
+)
+    def forward(self, x, noise_level_map):
+        downsampled_images = nn.PixelUnshuffle(2)(x) #reversible down sample images in 4 sub images
+        #### CHECKING DIMENSION OF THE NOISE LEVEL MAP ####
+        batch_size, _, h, w = downsampled_images.size()
+        noise_level_map = noise_level_map.view(batch_size, 1, h, w)   # reshape the noise level map to match dimension of down sample images if necessary
+        concat_input = torch.cat([downsampled_images, noise_level_map], dim=1) # concatenates the 4 downsampled images and the noise level map
+        return self.model(concat_input)
