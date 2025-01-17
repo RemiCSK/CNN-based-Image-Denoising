9b5005634de71730acdcd12deb967c8014bbbfb5
RemiCSK <remi.calvet1@gmail.com>
2025-01-17 12:49:05 +0100

    Script makes analysis for 100 epoch ffdnet inspired mnist

--- testing_mnist_ffdnet_inspired.py
+++ testing_mnist_ffdnet_inspired.py
@@ -77,7 +77,7 @@ def create_uniform_noise_level_map(batch_size, height, width, noise_std):
 
 
 ##### LOAD THE MODEL #####
-num_epochs = 10 # number of epoch of the already trained model
+num_epochs = 100 # number of epoch of the already trained model
 noise_std_train = 0.1 # amount of noise added during the training of the already trained model
 model_path = f'trained_models/FFDNet_inspired_small_mnist_{num_epochs}_epochs_{noise_std_train}_noise_std.pth'
 optimizer_path = f'trained_models/optimizer_FFDNet_inspired_small_mnist{num_epochs}_epochs_{noise_std_train}_noise_std.pth'
@@ -149,15 +149,25 @@ def results_test_presentation(noise_std_guess):
     Takes noise_std_guess as input and returns plots and errors of the denoising model on the test set.
     """
     ##### TEST ON THE TEST SET #####
+        ##### RUN AND COMPUTE THE LOSS ON THE TEST SET #####
+    criterion = nn.MSELoss()
 
-    # Run the model on the noisy images
+    test_loss = 0.0
     with torch.no_grad():
         b, _, h, w = noisy_images_train.size()   # to create noise level map and making sure dimensions match
         noise_level_map = create_uniform_noise_level_map(b, h, w, noise_std_guess)  #noise_std_guess because that's a guess of noise we think the images have. Then it creates a noise_level_map that we will give to the model.
-        denoised_images = FFDNet_inspired_small_mnist_model(noisy_images_test, noise_level_map)
+
+        denoised_images = torch.zeros_like(noisy_images_test).to(device) # initialize tensor so it doesn't count in the time to denoise
+        start_time = time.time()   # Note that the first time we call for our function, the time will be longer because of initialization of various tensors
+        denoised_images = FFDNet_inspired_small_mnist_model(noisy_images_test,noise_level_map)
+        end_time = time.time()
+        test_loss = criterion(denoised_images, images_test.to(device)).item()
+        time_taken = end_time - start_time
+    print(f'It took {time_taken} seconds to denoise {noisy_images_test.shape[0]} images from the test dataset and the MSE loss on those images is {test_loss:.4f}')
+
 
     # Plot the original, noisy, and denoised images
-    fig, axes = plt.subplots(3, 5, figsize=(15, 12))
+    fig, axes = plt.subplots(3, 5, figsize=(15, 13))
     for i in range(5):
         # Plot original images in the first row
         ax = axes[0, i]
@@ -182,31 +192,29 @@ def results_test_presentation(noise_std_guess):
              f"Using the model: FFDNet_inspired_small_mnist_{num_epochs}_epochs_{noise_std}_noise_std\n"
              f"True noise std added during training: {noise_std_train}\n"
              f"True noise std added to the clean images for testing is {noise_std}\n"
-             f"Additional input of the model is a uniform noise level map with value {noise_std_guess}.", fontsize=16)
+             f"Additional input of the model is a uniform noise level map with value {noise_std_guess}.\n"
+             f"It took {time_taken} seconds to denoise {noisy_images_test.shape[0]} images and the MSE loss on those images is {test_loss:.4f}", fontsize=16)
     plt.show()
 
-
-    ##### COMPUTE THE LOSS ON THE TEST SET #####
-    criterion = nn.MSELoss()
-
-    test_loss = 0.0
-    with torch.no_grad():
-        b, _, h, w = noisy_images_train.size()   # to create noise level map and making sure dimensions match
-        noise_level_map = create_uniform_noise_level_map(b, h, w, noise_std_guess)  #noise_std_guess because that's a guess of noise we think the images have. Then it creates a noise_level_map that we will give to the model.
-
-        start_time = time.time()
-        denoised_images_test = FFDNet_inspired_small_mnist_model(noisy_images_test,noise_level_map)
-        end_time = time.time()
-        test_loss = criterion(denoised_images_test, images_test.to(device)).item()
-        time_taken = end_time - start_time
-    print(f'It took {time_taken} seconds to denoise {noisy_images_test.shape[0]} images from the test dataset and the MSE loss on those images is {test_loss:.4f}')
-
+    # Reset the denoised_images and noise_level_map tensors otherwise, the first time we run this function it will be slower. This makes time measurements more consistent to make comparisons.
+    # Note that the first time we call for our function, the time will be longer because of initialization of various tensors
+    denoised_images = None
+    noise_level_map = None
     return
 
-#reminder, true noise std is 0.1
 
+
+
+#reminder, true noise std is 0.1
+print("DONT LOOK AT THE PLOT JUST BELOW, IT'S JUST A FIRST RUN TO INITIALIZE TENSORS")
+poubelle = results_test_presentation(1) # Note that the first time we call for our function, the time will be longer because of initialization of various tensors. That is why we make a first run that we will not use
+
+print("NOW YOU CAN LOOK AT THE FOLLOWING PLOTS")
+
+results_test_presentation(0.01) # we do a SUPER underestimation of the noise added to the images
+results_test_presentation(0.08) # we do an underestimation of the noise added to the images
 results_test_presentation(0.1) # noise_std_guess = 0.1 (matches the noised used for training)
 results_test_presentation(0.5) # we do an overstimation of the noise added to the images
-results_test_presentation(0.08) # we do an underestimation of the noise added to the images
-
 results_test_presentation(1) # we do a SUPER overstimation of the noise added to the images
+
+print("We see that underestimating or matching the noise level map gives good results. However, overestimating the noise level map gives poor results. We verified what the authors of FFDNet said in their article.")
